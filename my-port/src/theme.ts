d6ec3b5463480ba89aa67d425dac914934f682b1
MATEUS FELIPE ('JAPÃO') <129597459+Japazin@users.noreply.github.com>
2024-03-21 11:28:58 -0300

    theme

---
+++ my-port/src/theme.ts
@@ -0,0 +1,17 @@
+import { createTheme } from "@mui/material";
+
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#ffff",
+    },
+    secondary: {
+      main:"#ffff",
+    },
+  },
+  typography: {
+    fontFamily:"Segoe UI"
+  }
+});
+export default theme
